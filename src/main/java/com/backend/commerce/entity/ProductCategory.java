2f7b5c29cfc8eca972ca0be8f35016ec56da618a
Guilherme Lemes <guilhermelemes08@gmail.com>
2025-03-16 15:41:23 -0300

    feat: creation of project entities

---
+++ src/main/java/com/backend/commerce/entity/ProductCategory.java
@@ -0,0 +1,24 @@
+package com.backend.commerce.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Entity
+@Table(name = "product_category")
+@Getter
+@Setter
+public class ProductCategory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "category_name")
+    private String categoryName;
+
+    @OneToOne(cascade = CascadeType.ALL, mappedBy = "category")
+    private Set<Product> products;
+}
